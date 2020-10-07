88d1f8f700acbea8daabf8b2d263b8811db98ad7
sparkingdark <debomastet335@gmail.com>
2020-10-07 15:33:58 +0530

    done

---
+++ main.js
@@ -0,0 +1,5 @@
+import Factorial from './utils/factorial'
+
+const Facto = Factorial(n)
+
+exports.Facto = Facto
---
+++ utils/factorial.js
@@ -0,0 +1,12 @@
+const Factorial = (n)=>{
+      var fact = 1
+      for (let index = 1; index <= n; index++) {
+          fact*=index
+          
+      }
+
+      return fact
+
+}
+
+export default Factorial 
